e96ea90b190340a9b8d5c9398c2298da80c27e74
jaroshevskii <jaroshevskiisasha@gmail.com>
2023-04-27 23:08:31 +0300

    Add comments

--- Sources/BusTripsCLI/Route.swift
+++ Sources/BusTripsCLI/Route.swift
@@ -6,12 +6,27 @@
 ///
 /// `Route` contains the point of departure and arrival.
 struct Route {
-  /// The name of the route departure point.
+  /// The departure point of the route.
   let departure: String
 
-  /// The name of the arrival point of the route.
+  /// The arrival point of the route.
   let arrival: String
+
+  /// The separator for a string representation of the route.
+  static let separator = " - "
+
+  /// A string representation of the route.
+  var description: String { departure + Self.separator + arrival }
   
+  /// Creates a new route instance with the given departure and arrival points.
+  ///
+  /// - Parameters:
+  ///   - departure: The departure point of the route.
+  ///   - arrival: The arrival point of the route.
+  ///
+  /// - Throws:
+  ///   - `RouteError.emptyField` if the departure or arrival is an empty string.
+  ///   - `RouteError.sameDepartureAndArrival` if the departure and arrival are the same.
   init(departure: String, arrival: String) throws {
     if departure.isEmpty || arrival.isEmpty {
       throw RouteError.emptyField
@@ -23,17 +38,25 @@ struct Route {
     self.arrival = arrival
   }
 
-  init(_ text: String) throws {
-    let parts = text.split(separator: " - ")
-    guard parts.count == 2 else {
+  /// Creates a new route from the given string.
+  ///
+  /// - Parameter description: A string containing departure and arrival points separated by `Route.separator`.
+  ///
+  /// - Throws:
+  ///   - `RouteError.invalidFormat` if the description doesn't contain departure and arrival points separated by `Route.separator`.
+  ///   - Errors thrown by `init(departure:arrival:)`.
+  init(_ description: String) throws {
+    let routeComponents = description.split(separator: Self.separator)
+    guard routeComponents.count == 2 else {
       throw RouteError.invalidFormat
     }
     try self.init(
-      departure: parts.first!.trimmingCharacters(in: .whitespaces),
-      arrival: parts.last!.trimmingCharacters(in: .whitespaces))
+      departure: routeComponents.first!.trimmingCharacters(in: .whitespaces),
+      arrival: routeComponents.last!.trimmingCharacters(in: .whitespaces))
   }
 }
 
+/// An error that can be thrown when working with `Route`.
 enum RouteError: Error {
   case emptyField
   case sameDepartureAndArrival
